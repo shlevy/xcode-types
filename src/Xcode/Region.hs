e621cdb06cfe93f8f0ce3ce3ca2e82a189d6ac18
Shea Levy <shea@shealevy.com>
2011-09-01 21:59:26 -0400

    Add Xcode.Region

---
+++ src/Xcode/Region.hs
@@ -0,0 +1,23 @@
+{- |
+   Module :  Xcode.Region
+   License :  MIT
+
+   Maintainer :  shea@shealevy.com
+   Stability  :  unstable
+   Portability :  portable
+
+   An Xcode locale
+-}
+
+module Xcode.Region ( Region( .. ) ) where
+
+-- | An Xcode locale
+-- Locales will be added as they are encountered
+
+data Region
+  = English
+  | Japanese
+  | French
+  | German
+  deriving Show
+
