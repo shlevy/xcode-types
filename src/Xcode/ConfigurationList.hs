7219839254be54db468ed59eb14a79b89e71f6ba
Shea Levy <shea@shealevy.com>
2011-09-01 22:13:21 -0400

    Add Xcode.ConfigurationList

---
+++ src/Xcode/ConfigurationList.hs
@@ -0,0 +1,26 @@
+{- |
+   Module :  Xcode.ConfigurationList
+   License :  MIT
+
+   Maintainer :  shea@shealevy.com
+   Stability  :  unstable
+   Portability :  portable
+
+   A list of build configurations
+-}
+
+module Xcode.ConfigurationList ( ConfigurationList( .. ) ) where
+
+import Xcode.UUID ( UUID )
+import Xcode.BuildConfiguration ( BuildConfiguration )
+
+-- | A list of build configurations
+
+data ConfigurationList = ConfigurationList
+  { uuid                          :: UUID                    -- ^ The unique identifier for this object
+  , buildConfigurations           :: [ BuildConfiguration ]  -- ^ The configurations in the list
+  , defaultConfigurationIsVisible :: Bool                    -- ^ Is the default visible?
+  , defaultConfiguration          :: BuildConfiguration      -- ^ The default build configuraiton
+  }
+  deriving ( Show )
+
