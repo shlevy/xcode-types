8ccedafae0ba82d7128396a493bc0e08e335b742
Shea Levy <shea@shealevy.com>
2011-11-10 11:15:48 -0500

    Add Xcode.PBX.FileReference

---
+++ src/Xcode/PBX/FileReference.hs
@@ -0,0 +1,30 @@
+{- |
+   Module :  Xcode.PBX.FileReference
+   License :  MIT
+
+   Maintainer :  shea@shealevy.com
+   Stability  :  unstable
+   Portability :  portable
+
+   A project file
+-}
+
+module Xcode.PBX.FileReference ( FileReference( .. ) ) where
+
+import Xcode.FileEncoding ( FileEncoding )
+import Xcode.FileType ( FileType )
+import System.FilePath.Posix ( FilePath )
+import Xcode.PBX.SourceTree ( SourceTree )
+
+-- | A project file
+
+data FileReference = FileReference
+  { uuid               :: UUID                -- ^ The unique identifier for this object
+  , fileEncoding       :: Maybe FileEncoding  -- ^ The encoding of the file
+  , fileType           :: FileType            -- ^ The type of file
+  , fileTypeIsExplicit :: Bool                -- ^ Whether the file type was explicit or inferred
+  , includeInIndex     :: Bool                -- ^ Whether to include the file in the file index
+  , path               :: FilePath            -- ^ The path to the file
+  , sourceTree         :: SourceTree          -- ^ The source tree of the file
+  }
+
