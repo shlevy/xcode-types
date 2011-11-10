b801c28452c127c6c9791b06b70a532be5d0cd2f
Shea Levy <shea@shealevy.com>
2011-11-10 15:00:59 -0500

    Use BuildPhase in AggregateTarget

--- src/Xcode/PBX/AggregateTarget.hs
+++ src/Xcode/PBX/AggregateTarget.hs
@@ -12,17 +12,17 @@
 module Xcode.PBX.AggregateTarget ( AggregateTarget( .. ) ) where
 
 import Xcode.ConfigurationList ( ConfigurationList )
-import Xcode.PBX.ShellScriptBuildPhase ( ShellScriptBuildPhase )
+import Xcode.PBX.BuildPhase ( BuildPhase )
 import Xcode.PBX.TargetDependency ( TargetDependency )
 
 -- | An aggregate of project targets
 
 data AggregateTarget = AggregateTarget
-  { uuid                   :: UUID                      -- ^ The unique identifier for this object
-  , buildConfigurationList :: ConfigurationList         -- ^ The build configurations
-  , buildPhases            :: [ ShellScriptBuildPhase ] -- ^ The phases to build the target
-  , dependencies           :: [ TargetDependency ]      -- ^ The dependencies of this target
-  , name                   :: String                    -- ^ The name of the target
-  , productName            :: String                    -- ^ The name of the product
+  { uuid                   :: UUID                  -- ^ The unique identifier for this object
+  , buildConfigurationList :: ConfigurationList     -- ^ The build configurations
+  , buildPhases            :: [ BuildPhase ]        -- ^ The phases to build the target
+  , dependencies           :: [ TargetDependency ]  -- ^ The dependencies of this target
+  , name                   :: String                -- ^ The name of the target
+  , productName            :: String                -- ^ The name of the product
   }
 
