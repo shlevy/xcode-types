9c6d004afde46e5e0fb01da60665e220dfd98252
Shea Levy <shea@shealevy.com>
2011-11-10 00:14:24 -0500

    Add Xcode.Attribute

---
+++ src/Xcode/Attribute.hs
@@ -0,0 +1,20 @@
+{- |
+   Module :  Xcode.Attribute
+   License :  MIT
+
+   Maintainer :  shea@shealevy.com
+   Stability  :  unstable
+   Portability :  portable
+
+   A miscellaneous attribute
+-}
+
+module Xcode.Attribute ( Attribute( .. ) ) where
+
+-- | A miscellaneous attribute
+-- Attributes will be added as they encountered
+
+data Region
+  = OrganizationName
+  deriving Show
+
