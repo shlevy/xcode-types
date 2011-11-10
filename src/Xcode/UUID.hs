4221a5732173c5b6234cdde33c4ec331f3fe9bff
Shea Levy <shea@shealevy.com>
2011-11-10 00:46:13 -0500

    Add Xcode.UUID

---
+++ src/Xcode/UUID.hs
@@ -0,0 +1,18 @@
+{- |
+   Module :  Xcode.UUID
+   License :  MIT
+
+   Maintainer :  shea@shealevy.com
+   Stability  :  unstable
+   Portability :  portable
+
+   A UUID for Xcode elements
+-}
+
+module Xcode.UUID ( UUID( .. ) ) where
+
+import Data.LargeWord ( Word96 )
+
+-- | A UUID for Xcode elements
+newtype UUID = UUID Word96
+
